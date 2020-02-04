2da5a7a0496d16e092af6b9d2e328008dfeeb506
rsm <esaliev9898@gmail.com>
2020-02-04 22:09:27 +0600

    qwe

--- app/Template.php
+++ app/Template.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Template extends Model
 {
-    protected $fillable = ['user_id','template'];
+    protected $fillable = ['user_id','template','finger_id'];
 }
