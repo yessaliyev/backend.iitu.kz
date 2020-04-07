1f3e1b02d7771f8c6c15de468cea7c93583590a1
Aidar <kanybek.a13@gmail.com>
2020-04-07 20:06:57 +0600

    fix SentTemplate model not found

--- app/Http/Controllers/Api/TemplateController.php
+++ app/Http/Controllers/Api/TemplateController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\SentTemplate;
+use App\Models\Templates\SentTemplate;
 use Illuminate\Http\Request;
 use App\Models\Templates\Template;
 
