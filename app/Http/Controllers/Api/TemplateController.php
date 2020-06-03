7760801c90ffafff0a84281a360a43436c22e37a
yessaliyev <40271311+yessaliyev@users.noreply.github.com>
2020-06-03 20:18:30 +0600

    fix get by room

--- app/Http/Controllers/Api/TemplateController.php
+++ app/Http/Controllers/Api/TemplateController.php
@@ -13,8 +13,8 @@ class TemplateController extends Controller
     public function set(Request $request)
     {
         $request->validate([
-            'user_id'=>'required',
-            'template'=>'required',
+            'user_id' => 'required',
+            'template' => 'required',
             'finger_id' => 'required'
         ]);
 
@@ -30,14 +30,25 @@ class TemplateController extends Controller
         $request->validate(['room_id' => 'required|integer']);
         if (!isset($request->test)) $request->test = false;
 
-        $templates = Template::getByRoom($request->room_id,$request->test);
-        return $templates;
+        $templates = Template::getByRoom($request->room_id, $request->test);
+
+        $sent = SentTemplate::where('data', json_encode($templates, JSON_UNESCAPED_UNICODE))->first();
+
+        if (!empty($sent) and !empty(json_decode($sent->data))) {
+            return response([
+                'new' => false,
+                'templates' => json_decode($sent->data)
+
+            ]);
+        }
+
         $sent = new SentTemplate();
         $sent->room_id = $request->room_id;
-        $sent->data = json_encode($templates,JSON_UNESCAPED_UNICODE);
+        $sent->data = json_encode($templates, JSON_UNESCAPED_UNICODE);
         $sent->save();
 
         return response([
+            'new' => true,
             'templates' => $templates
         ]);
     }
--- app/Models/Templates/Template.php
+++ app/Models/Templates/Template.php
@@ -10,30 +10,28 @@ class Template extends Model
 {
     protected $fillable = ['user_id', 'template', 'finger_id'];
 
-    public static function getByRoom($room_id,$test = false)
+    public static function getByRoom($room_id, $test = false)
     {
         date_default_timezone_set('Asia/Almaty');
-        $start = time();
-        $end = time() + 660;
-
-        if ($test){
-            $start = 0;
-            $end = time()+157220;
+        $start = date('Y-m-d H:i:s', strtotime('-2 minutes'));
+        $end = date('Y-m-d H:i:s', strtotime('+2 minutes'));
+//        return [$start,$end];
+        if ($test) {
+            $start = date('Y-m-d H:i:s', strtotime('-2 weeks'));
+            $end = date('Y-m-d H:i:s', strtotime('+2 weeks'));
         }
 
         return DB::select(
-            'select *
+            "select *
                     from templates
                     where user_id in (select user_id
                       from students
                                left join users u on students.user_id = u.id
                                left join groups g on students.group_id = g.id
                                left join lessons l on g.id = l.group_id
-                      where room_id = 5
-                        and l.date between
-                        (SELECT TIMESTAMP \'epoch\' + '.$start.' * INTERVAL \'1 second\')
-                        and (SELECT TIMESTAMP \'epoch\' + '.$end.' * INTERVAL \'1 second\')
-            )'
+                      where room_id = " . $room_id . " and l.date between '" . $start . "'
+                      and '" . $end . "'
+            )"
         );
 
 
