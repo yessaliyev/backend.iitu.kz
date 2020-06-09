8fdc4438a4b2424267d89ffc571b30d990c6e12a
yessaliyev <40271311+yessaliyev@users.noreply.github.com>
2020-06-10 00:48:38 +0600

    fix get title of project

--- app/Http/Controllers/Api/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -9,34 +9,38 @@ use App\Models\Templates\SentTemplate;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AttendanceController extends Controller
 {
     public function set(Request $request)
     {
-        $validate_data = $request->validate([
-           'room_id'=>'required',
-           'index'=>'required'
+        $request->validate([
+            'room_id' => 'required',
+            'index' => 'required',
         ]);
 
-        $sent_data = SentTemplate::where('room_id' , $request->room_id)->orderBy('id','desc')->first();
+        if (!isset($request->status)) $request->status = 1;
+
+        $sent_data = SentTemplate::where('room_id', $request->room_id)->orderBy('id', 'desc')->first();
         if (empty($sent_data)) return response(['message' => 'not found']);
-        $student = User::findOrFail(json_decode($sent_data->data,true)[$request->index]['user_id'])->student;
+        $student = User::findOrFail(json_decode($sent_data->data, true)[$request->index]['user_id'])->student;
         $lesson = $student->group->currentLesson();
-        $att = new Attendance();
-        $att->lesson_id = $lesson->id;
-        $att->student_id = $student->id;
-        $att->status = 1;
-        $att->save();
+
+        throw_if(empty($request->group_id), new NotFoundHttpException('current lesson not found'));
+
+        Attendance::updateOrCreate([
+            'lesson_id' => $lesson->id,
+            'student_id' => $student->id], ['status' => $request->status]);
 
         return response([
-           'message' => "OK!"
+            'message' => "OK!"
         ]);
     }
 
     public function getCourseAttendance()
     {
-        return Attendance::teacherLessons(Auth::user()->teacher->id,10);
+        return Attendance::teacherLessons(Auth::user()->teacher->id, 10);
     }
 
     public function getGroupAttendance(Request $request)
